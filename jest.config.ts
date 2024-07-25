73d6a2e2c00a64eae6971d1aeb9db256d62e80d5
Jamshidbek Makhmudov <101079499+Jamshidbek-Makhmudov@users.noreply.github.com>
2024-07-25 15:26:33 +0900

    added jest configuration

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from '@jest/types';
+
+const baseDir = '<rootDir>/src/**/*.ts';
+const baseTestDir = '<rootDir>/src/test';
+ 
+const config: Config.InitialOptions = {
+  preset: 'ts-jest', 
+  testEnvironment: 'node',
+  verbose: true,
+  collectCoverage: true,
+  roots: ['<rootDir>/tests'],
+  setupFiles: ['<rootDir>/tests/setup.ts'],
+    collectCoverageFrom: [
+      `${baseDir}/**/*.ts`, '!**/node_modules/**'
+    ],
+
+}
+
+export default config;
